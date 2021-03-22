ecfbbf16f32aaea06a61768630f5038982c637d6
cynosure2k21 <80642902+cynosure2k21@users.noreply.github.com>
2021-03-22 18:18:15 +0530

    Add files via upload

---
+++ js/validateInterview.js
@@ -0,0 +1,43 @@
+var interviewForm = document.getElementById("inter-battle-view");
+
+interviewForm.addEventListener("submit", function (event) {
+	var name = document.getElementById("name");
+	var email = document.getElementById("email");
+	var college = document.getElementById("college");
+	var course = document.getElementById("course");
+	var year = document.getElementById("year");
+	var phone = document.getElementById("phone");
+	var resume = document.getElementById("file1");
+	var profile = document.getElementsByName("job");
+	var selectedProfile = "";
+	for (i = 0; i < profile.length; i++) {
+		if (profile[i].checked) selectedProfile = profile[i].value;
+	}
+
+	fetch("https://cynosure-admin.herokuapp.com/api/register", {
+		method: "post",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			eventId: "inter-battle-view",
+			name: name.value,
+			email: email.value,
+			phoneNumber: phone.value,
+			course: course.value,
+			year: year.value,
+			college: year.value,
+			resumeURL: resume.value,
+			selectedProfile,
+		}),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			alert(data.message);
+		})
+		.catch((err) => {
+			console.log(err);
+			alert("Oppsss!!! Try again");
+		});
+	event.preventDefault();
+});
---
+++ js/validateLab.js
@@ -0,0 +1,55 @@
+var labForm = document.getElementById("tech-labyrinth");
+
+labForm.addEventListener("submit", function (event) {
+	var teamName = document.getElementById("teamName");
+	var name = document.getElementById("name");
+	var email = document.getElementById("email");
+	var college = document.getElementById("college");
+	var course = document.getElementById("course");
+	var year = document.getElementById("year");
+	var phone = document.getElementById("phone");
+	var instId = document.getElementById("instaId");
+
+	var name2 = document.getElementById("name2");
+	var email2 = document.getElementById("email2");
+	var college2 = document.getElementById("college2");
+	var course2 = document.getElementById("course2");
+	var year2 = document.getElementById("year2");
+	var phone2 = document.getElementById("phone2");
+	var instId2 = document.getElementById("instaId2");
+
+	fetch("https://cynosure-admin.herokuapp.com/api/register", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			eventId: "tech-labyrinth",
+			name: name.value,
+			email: email.value,
+			phoneNumber: phone.value,
+			course: course.value,
+			year: year.value,
+			college: college.value,
+			instaID: instId.value,
+			teamName: teamName.value,
+			teamMember: name2.value,
+			teamMemberCollege: college2.value,
+			teamMemberYear: year2.value,
+			teamMemberCourse: course2.value,
+			teamMemberPhoneNumber: phone2.value,
+			teamMemberEmail: email2.value,
+			memberInstaID: instId2.value,
+		}),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			alert(data.message);
+		})
+		.catch((err) => {
+			console.log(err);
+			alert("Oppsss!!! Try again");
+		});
+
+	event.preventDefault();
+});
